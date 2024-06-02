1096ba1269110202bda7b6dad0b2b2c899c3f58d
yosuanetzme18 <59557434+Ganasa18@users.noreply.github.com>
2024-06-02 13:53:22 +0700

    update: global service initial

---
+++ lib/global.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Global {
+  static Future init() async {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // transparent status bar
+    ));
+    WidgetsFlutterBinding.ensureInitialized();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todo_riverpod/app/app.dart';
+import 'package:flutter_todo_riverpod/global.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  await Global.init();
   runApp(const ProviderScope(
     child: TodoApp(),
   ));
